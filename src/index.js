2482567f3f7ef51fcf9bbf71b586fb590c6c7d68
siddht1 <rt1yrru+github@gmail.com>
2023-02-01 12:42:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,3 +17,28 @@ app.use("/api/v1/trains", v1TrainsRouter);
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
 });
+
+
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
+const handler = (req, res) => {
+  const d = new Date()
+  res.end(d.toString())
+}
+
+module.exports = allowCors(handler)
