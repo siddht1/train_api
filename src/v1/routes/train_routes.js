911bb3868d3c4f325211baf6a59cefde68be6109
siddht1 <rt1yrru+github@gmail.com>
2023-02-01 12:15:31 +0530

    Update and rename trainRoutes.js to train_routes.js

---
+++ src/v1/routes/train_routes.js
@@ -0,0 +1,16 @@
+// In src/v1/routes/train_routes.js
+const express = require("express");
+const train_Controller = require("../../controllers/train_Controller");
+const router = express.Router();
+
+
+router.get("/", train_Controller.getAllTrains);
+
+router.get("/:traintId", train_Controller.getOneTrain);
+
+router.post("/", train_Controller.createNewTrain);
+
+router.patch("/:trainId", train_Controller.updateOneTrain);
+
+router.delete("/:trainId", train_Controller.deleteOneTrain);
+module.exports = router;
