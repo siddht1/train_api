13645eb7351046bbd19f5ad0e63d3601a0951792
siddht1 <rt1yrru+github@gmail.com>
2023-02-01 12:46:06 +0530

    Update index.js

--- src/v1/routes/index.js
+++ src/v1/routes/index.js
@@ -3,7 +3,18 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get((req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   res.send(`<h2>Hello from ${req.baseUrl}</h2>`);
 });
 
 module.exports = router;
+
+
