5d42c3de8548b154de1083b6172d15b30d763f5a
siddht1 <rt1yrru+github@gmail.com>
2023-02-02 09:48:35 +0530

    Update train_routes.js

--- src/v1/routes/train_routes.js
+++ src/v1/routes/train_routes.js
@@ -3,6 +3,16 @@ const express = require("express");
 const train_Controller = require("../../controllers/train_Controller");
 const router = express.Router();
 
+ res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+
 
 router.get("/", train_Controller.getAllTrains);
 
