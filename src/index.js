170d94443fb7df0584f89c966b1e70778d0a6f78
siddht1 <rt1yrru+github@gmail.com>
2023-06-17 08:37:42 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,7 @@ app.use("/api/v1", v1Router);
 // *** ADD ***
 app.use("/api/v1/trains", v1TrainsRouter);
 
-var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-res.send({"status":"OK","API":"WORKING @ V1","API_V1":"/API/V1","url":fullUrl,"CORS":"allowed"});
+
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
